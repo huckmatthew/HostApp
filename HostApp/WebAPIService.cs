58b578eff8da9566ceaeca0237727c16e2cfc9ce
Matt Huck <Matt.Huck@benaissance.com>
2016-09-02 21:37:01 -0500

    Remove Sample Service1

--- HostApp/WebAPIService.cs
+++ HostApp/WebAPIService.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Autofac;
 using HostApp.BootStrap;
 using log4net;
